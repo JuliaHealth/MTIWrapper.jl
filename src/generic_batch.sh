c6a2faba18bc173b7010ca7341b15dedd307b133
Isabel Restrepo <isabelrestre@gmail.com>
2019-02-22 11:33:10 -0500

    my git is a mess

--- src/generic_batch.sh
+++ src/generic_batch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-email=$1
-username=$2
-password=$3
-in_file=$4
-out_file=$5
+MTI_DIR=$1
+email=$2
+username=$3
+password=$4
+in_file=$5
+out_file=$6
 
-./IIWebAPI/SKR_Web_API_V2_3/run.sh GenericBatchCustom --email $email $in_file --username $username --password $password > $out_file
+${MTI_DIR}/SKR_Web_API_V2_3/run.sh GenericBatchCustom --email $email $in_file --username $username --password $password > $out_file

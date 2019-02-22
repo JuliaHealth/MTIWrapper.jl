0b2084d57114d4d49e818957058922c04cdf44aa
Isabel Restrepo <isabelrestre@gmail.com>
2019-02-22 13:41:44 -0500

    mti working with input dir

--- src/MTIWrapper.jl
+++ src/MTIWrapper.jl
@@ -58,25 +58,37 @@ module MTIWrapper
     function install_web_api(root_dir)
 Installs MTI Java jar to specified location
 """
-function install_web_api(root_dir)
+function install_web_api(root_dir; api_version="SKR_Web_API_V2_3")
+
+    cwd = dirname(@__FILE__)
+
+    api_dir = "$root_dir/$api_version"
 
     if !isdir(root_dir)
         mkdir(root_dir)
     else
-        rm(root_dir, recursive = true, force = true)
-        mkdir(root_dir)
+        if isdir(api_dir)
+           rm(api_dir, recursive = true, force = true)
+           rm("$api_dir.jar")
+        end
+        # mkdir(root_dir)
     end
-
 
     println("Dowloading fresh copy of sources")
     # get sources and expand
-    download("https://ii.nlm.nih.gov/Web_API/SKR_Web_API_V2_3.jar", "$root_dir/SKR_Web_API_V2_3.jar")
-    run(`java sun.tools.jar.Main xf $root_dir/SKR_Web_API_V2_3.jar`)
+    cd(root_dir)
+    download("https://ii.nlm.nih.gov/Web_API/$api_version.jar", "$api_version.jar")
+    run(`java sun.tools.jar.Main xf $api_version.jar`)
 
+    # We have modified our class to pass credentials as an input - we'll put it in examples folder
+    # so that MTI default compilation scripts work
+    cp("$cwd/GenericBatchCustom.java", "$api_dir/examples/GenericBatchCustom.java") 
     # compile
-    cd("SKR_Web_API_V2_3")
-    run(`chmod +x ./compile.sh ./run.sh ./build.sh`)
-    run(`./compile.sh ../../GenericBatchCustom.java`)
+    # cd("SKR_Web_API_V2_3")
+    run(`chmod +x $api_dir/compile.sh $api_dir/run.sh $api_dir/build.sh`)
+    run(`$api_dir/compile.sh $api_dir/examples/GenericBatchCustom.java`)
+
+    cd(cwd)
 
 end
 
@@ -85,7 +97,7 @@ end
 Send a batch query to MTI. Use function `abstracts_to_request_file` to generate `in_file`
 """
 function mti_batch_query(mti_java_dir, email, username, password, in_file, out_file)
-    cwd= dirname(@__FILE__)
+    cwd = dirname(@__FILE__)
     run(`$cwd/generic_batch.sh $mti_java_dir $email $username $password $in_file $out_file`)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,9 @@
 using MTIWrapper
-using Base.Test
+using Test
 
-# write your own tests here
-@test 1 == 2
+cwd = dirname(@__FILE__)
+rm("$cwd/mti_results_work.txt")
+mti_dir = "$cwd/tmp"
+MTIWrapper.install_web_api(mti_dir)
+MTIWrapper.mti_batch_query(mti_dir, ENV["NCBI_EMAIL"], ENV["UMLS_USER"], ENV["UMLS_PSSWD"], 
+                           "$cwd/abstracts.txt", "$cwd/mti_results_work.txt")
